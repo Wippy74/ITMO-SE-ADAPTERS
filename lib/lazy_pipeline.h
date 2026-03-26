ffaab1387f5f81fc3600180f8a23f56ad566ecd3
Wippy74 <vsevoloduzaninov@gmail.com>
2026-03-26 21:47:24 +0300

    Implemented evaluate method which calls lazy function and accsess method to return pointer on container

--- lib/lazy_pipeline.h
+++ lib/lazy_pipeline.h
@@ -16,6 +16,7 @@ private:
       using Container = C<T, Args...>;
     }
     using Func = std::function<void>(C&);
+
     Data(const Func& func) : cont_ptr_(new Container{}), func_ptr_(std::make_unique<Func>(func)), IsOwner_(true) {}
     Data(Container& c) : cont_ptr_(&c) {}
     ~Data() {
@@ -23,11 +24,38 @@ private:
         delete cont_prt_;
       }
     }
+
+    void evaluate() {
+      if (func_ptr_) {
+        func_ptr_->operator()(*cont_ptr_);
+        func_ptr_.reset();
+      }
+    }
+
+    Container& Accsess(){
+      evaluate();
+      return *cont_ptr;
+    }
   private:
     Container* cont_ptr_;
     std::unique_ptr<Func> func_ptr_;
     bool IsOwner_;
-  }
+  };
+
+public:
+  using Type = Data::Type;
+  using value_type = Type;
+  using Contrainer = Data::Container;
+
+  LazyPipeline() : LazyPipeline(Contrainer{}) {}
+
+  LazyPipeline(const Data::Func& func) : data_(std::make_shared<Data>(func)) {}
+
+  template <typename Cont>
+  requires std::same_as<const Cont, Contrainer>
+  LazyPipeline(const Cont& c) : data_(std::make_shared<Data>(c)) {}
+
+
 private:
   std::shared_ptr<Data> data_;
 };
