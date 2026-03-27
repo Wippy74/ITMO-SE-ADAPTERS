3ca9e764b825e4621ccb9ba46b9c40bb0dcf4e2f
Wippy74 <vsevoloduzaninov@gmail.com>
2026-03-28 00:10:07 +0300

    Implemented AsDataFlow adapter which creates lazy pipeline which references an existing container

--- lib/as_data_flow.h
+++ lib/as_data_flow.h
@@ -1,1 +1,16 @@
-concept;
+#pragma once
+
+#include "lazy_pipeline.h"
+#include "traits.h"
+
+#include <utility>
+
+template <typename ValueType, template <typename, typename...> typename Container, typename... Args>
+auto AsDataFlow(Container<ValueType, Args...>& container) {
+  return LazyPipeline<ValueType, Container, Args...>(container);
+}
+
+template <typename ValueType, template <typename, typename...> typename Container, typename... Args>
+auto AsDataFlow(const Container<ValueType, Args...>& container) {
+  return LazyPipeline<const ValueType, Container, Args...>(container);
+}
