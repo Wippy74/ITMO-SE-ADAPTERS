66fb2061e406f1a2b66ae213f91ab272076e0984
Wippy74 <vsevoloduzaninov@gmail.com>
2026-03-30 03:43:15 +0300

    Fixed dangling reference made by Make method

--- lib/split_expected.h
+++ lib/split_expected.h
@@ -6,25 +6,52 @@ template <template <typename, typename...> typename Container, typename... Args>
 class SplitExpectedAdapter {
 public:
   template <typename Data>
-  auto Apply(Data& data_flow) {
-    auto error_stream = data_flow.template Make<typename Data::Type::error_type, Container, Args...>(
-                                                [](auto& input, auto& error_output) {
-                                                  for (auto& result : input) {
-                                                    if (!result) {
-                                                      error_output.push_back(result.error());
-                                                    }
-                                                  }
-                                                });
+  auto Apply(Data& data) {
+    using ExpectedType = typename Data::Type;
+    using ValueType = typename ExpectedType::value_type;
+    using ErrorType = typename ExpectedType::error_type;
+    using GetterType = typename  std::function<typename Data::Container&()>;
+
+    struct SplitCache {
+      GetterType getter;
+      std::vector<ValueType> values;
+      std::vector<ErrorType> errors;
+      bool processed = false;
+      
+      explicit SplitCache(GetterType g) : getter(std::move(g)) {}
+      
+      void EnsureProcessed() {
+        if (processed) {
+          return;
+        }
+        processed = true;
+        for (auto& result : getter()) {
+          if (result.has_value()) {
+            values.push_back(std::move(result.value()));
+          } else {
+            errors.push_back(std::move(result.error()));
+          }
+        }
+      }
+    };
+
+    auto cache = std::make_shared<SplitCache>(data.State());
+
+    auto error_stream = LazyPipeline<ErrorType, std::vector>(
+      [cache](std::vector<ErrorType>& output) {
+        cache->EnsureProcessed();
+        output = cache->errors;
+      }
+    );
         
-    auto value_stream = data_flow.template Make<typename Data::Type::value_type, Container, Args...>(
-                                                [](auto& input, auto& value_output) {
-                                                  for (auto& result : input) {
-                                                    if (result) {
-                                                      value_output.push_back(result.value());
-                                                    }
-                                                  }
-                                                });
-    return std::pair{error_stream, value_stream};
+    auto value_stream = LazyPipeline<ValueType, std::vector>(
+      [cache](std::vector<ValueType>& output) {
+        cache->EnsureProcessed();
+        output = cache->values;
+      }
+    );
+    
+    return std::pair{std::move(error_stream), std::move(value_stream)};
   }
 };
 
